97ee0bc9247c2c55c1ddf3dbc047cf14f829204b
Justin Clayton <jt.clayton92@yahoo.com>
2020-07-09 13:06:37 -0700

    Complete back end logic for getting gold from form post, add reset option.

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('process_gold', views.process_gold),
+    path('reset', views.reset)
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from random import randint
+from datetime import datetime
 
 # Create your views here.
 def index(request):
+  if not 'logs' in request.session:
+    request.session['logs'] = []
+  if not 'gold' in request.session:
+    request.session['gold'] = 0
   return render(request, 'index.html')
 
+def process_gold(request):
+  goldMap = {'farm': [10,20], 'cave': [5,10], 'house': [2,5], 'casino': [-50,50]}
+  location = request.POST.get('location')
+  bounds = goldMap[location]
+  result = randint(bounds[0], bounds[1])
+  request.session['gold'] += result
+  log = {
+    'amount': abs(result),
+    'isGain': result >= 0,
+    'location': location,
+    'time': datetime.now().strftime('%H:%M %p %d/%m/%y')
+  }
+  print(log)
+  request.session['logs'].insert(0, log)
+  print(request.session['logs'])
+  return redirect(index)
+
+def reset(request):
+  request.session.clear()
+  return redirect('/')
